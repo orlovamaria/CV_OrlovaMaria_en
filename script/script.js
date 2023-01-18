8bbb76880376d0334591bd5e1ee523613f45c0a5
orlovamaria <88633050+orlovamaria@users.noreply.github.com>
2023-01-18 20:54:17 +0300

    init

---
+++ script/script.js
@@ -0,0 +1,52 @@
+const images = document.querySelectorAll('.slider-img')
+const sliderLine = document.querySelector('.slider-line')
+
+let count = 0;
+let width;
+
+const rollSlider = () => {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)'
+}
+
+const init = () => {
+    width = document.querySelector('.main-slider').offsetWidth
+    sliderLine.style.width = width*images.length + 'px'
+    images.forEach(elem => {
+        elem.style.width = width + 'px';
+    })
+    rollSlider();
+}
+window.addEventListener('resize', init)
+init()
+
+document.querySelector('.button-prev').addEventListener('click', function(){
+    count--;
+    if(count < 0){
+        count = images.length - 1
+    }
+    rollSlider()
+})
+
+
+document.querySelector('.button-next').addEventListener('click', function(){
+    count++;
+    if(count >= images.length){
+        count = 0
+    }
+    rollSlider()
+})
+
+
+const headerBurger = document.querySelector('.main-header-burger')
+const mainHeaderNav = document.querySelector('.main-header-nav')
+
+headerBurger.addEventListener('click', function(){
+    headerBurger.classList.toggle('active')
+    mainHeaderNav.classList.toggle('active')
+})
+
+mainHeaderNav.addEventListener('click', function(){
+    mainHeaderNav.classList.remove('active')
+    headerBurger.classList.remove('active')
+})
+
